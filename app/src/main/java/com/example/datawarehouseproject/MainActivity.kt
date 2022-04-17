557b0c3572b509090e87e18cd4aeda8ad06a7140
Oguzhan Karakaya <7oguzhankarakaya@gmail.com>
2022-04-17 18:03:00 +0300

    ilk sayfa ve sayfa geçişleri ayarlandı

--- app/src/main/java/com/example/datawarehouseproject/MainActivity.kt
+++ app/src/main/java/com/example/datawarehouseproject/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.datawarehouseproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.datawarehouseproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ActivityMainBinding()
+        whenPushNextButton()
+    }
+
+    private fun whenPushNextButton()
+    {
+        binding.button1.setOnClickListener()
+        {
+            val intent = Intent(this, SelectionScreenActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+        }
+    }
+    private fun ActivityMainBinding()
+    {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
