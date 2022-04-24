6a14bfcac4d69d8e40493b9d8079ef868787af28
mertgultekin <mrtgltkn.101@gmail.com>
2022-04-24 14:51:40 +0300

    donut chart

--- app/src/main/java/com/example/datawarehouseproject/ResultActivity.kt
+++ app/src/main/java/com/example/datawarehouseproject/ResultActivity.kt
@@ -1,8 +1,11 @@
 package com.example.datawarehouseproject
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import app.futured.donut.DonutProgressView
+import app.futured.donut.DonutSection
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +13,21 @@ class ResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_result)
 
         val resultTextView = findViewById<TextView>(R.id.resultTextView)
-        resultTextView.text = SpinnerObjects.school
+
+        val donut_view = findViewById<DonutProgressView>(R.id.donut_view)
+        var resultint: Int=10
+
+        var resultFloat =resultint.toFloat()
+        resultTextView.text = resultFloat.toString()
+
+        val section1 = DonutSection(
+            name = "section_1",
+            color = Color.parseColor("#FB1D32"),
+            amount = resultFloat
+        )
+
+        donut_view.cap = 20f
+        donut_view.submitData(listOf(section1))
 
     }
 }
